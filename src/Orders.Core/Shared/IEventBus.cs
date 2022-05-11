239aeed1078ddc6ddffc56afe66be015a97e7427
Amir <amir.a.rezaei.p@gmail.com>
2022-05-12 01:36:44 +0430

    Delete unnecessary files.

--- src/Orders.Core/Shared/IEventBus.cs
+++
@@ -1,9 +0,0 @@
-﻿// namespace Orders.Core.Shared;
-// //     // todo: if another micro was in the picture 
-//
-// public interface IEventBus
-// {
-//     void Publish(IIntegrationEvent @event);
-//
-//     void Subscribe<T>() where T : IIntegrationEvent;
-// }
